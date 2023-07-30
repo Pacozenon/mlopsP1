9767c063481b719aea1ba6243224c5b06aa2a23b
Pacozenon <paco.zenon@gmail.com>
2023-07-29 22:54:37 -0600

    precommit w/ruff

--- session-5/iris.py
+++ session-5/iris.py
@@ -1,39 +1,32 @@
-import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris  # Import in incorrect order
+
+import numpy as np  # Import in incorrect order
+
+from sklearn.linear_model import LogisticRegression  # Import in incorrect order
 
 # Load data from sklearn
 X, y = load_iris(return_X_y=True)
 
 # Train the model using regresion logistic
-clf = LogisticRegression(solver='lbfgs',
-                         max_iter=1000,
-                         multi_class='multinomial').fit(X, y)
+clf = LogisticRegression(solver="lbfgs", max_iter=1000, multi_class="multinomial").fit(
+    X, y
+)
 # Define iris types
-<< << << < HEAD
-iris_type = {0: 'setosa', 1: 'versicolor', 2: 'virginica'
-             }
-== == == =
-iris_type = {
-    0: 'setosa',
-    1: 'versicolor',
-    2: 'virginica'
-}
->>>>>> > d2ec574135b3f53215db217593e5b435cd9e85b1
+iris_type = {0: "setosa", 1: "versicolor", 2: "virginica"}
 
 
 # Define dummy values
 sepal_length, sepal_width, petal_length, petal_width = 2, 3, 4, 6
 
-<< << << < HEAD
-
-== == == =
->>>>>> > d2ec574135b3f53215db217593e5b435cd9e85b1
 X = [sepal_length, sepal_width, petal_length, petal_width]
 
 
 # Make a prediction
 
 prediction = clf.predict_proba([X])
-print({'class': iris_type[np.argmax(prediction)],
-      'probability': round(max(prediction[0]), 2)})
+print(
+    {
+        "class": iris_type[np.argmax(prediction)],
+        "probability": round(max(prediction[0]), 2),
+    }
+)
